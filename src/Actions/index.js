a8e4f5251df1975641d386e5de8af6788c5a15d4
LilPup <a1603171.myy.haaga-helia.fi>
2018-07-21 20:21:02 +0300

    Fix local time by toLocaleString()

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,53 +1,53 @@
-import axios from 'axios';
-const ROOT_URL = 'https://mypostapi.herokuapp.com/api/posts';
+import axios from "axios";
+const ROOT_URL = "https://mypostapi.herokuapp.com";
 // const ROOT_URL = 'http://localhost:8080/api/posts';
 
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const CREATE_POST = 'CREATE_POST';
-export const FETCH_POST = 'FETCH_POST';
-export const DELETE_POST = 'DELETE_POST';
-export const EDIT_POST = 'EDIT_POST';
+export const FETCH_POSTS = "FETCH_POSTS";
+export const CREATE_POST = "CREATE_POST";
+export const FETCH_POST = "FETCH_POST";
+export const DELETE_POST = "DELETE_POST";
+export const EDIT_POST = "EDIT_POST";
 
 export const fetchPosts = () => {
-    const request = axios.get(`${ROOT_URL}`);
-    return {
-        type: FETCH_POSTS,
-        payload: request
-    }
-}
+  const request = axios.get(`${ROOT_URL}/api/posts`);
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+};
 
 export function createPost(newPost) {
-    const request = axios.post(`${ROOT_URL`, newPost);
-    return {
-        type: CREATE_POST,
-        payload: request
-    }
+  const request = axios.post(`${ROOT_URL}/api/posts`, newPost);
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
 }
 
 export function fetchPost(postId) {
-    const request = axios.get(`${ROOT_URL}/${postId}`);
-    return {
-        type: FETCH_POST,
-        payload: request
-    }
+  const request = axios.get(`${ROOT_URL}/api/posts/${postId}`);
+  return {
+    type: FETCH_POST,
+    payload: request
+  };
 }
 
 export function deletePost(postId) {
-    const request = axios.delete(`${ROOT_URL}/${postId}`);
-    return {
-        type: DELETE_POST,
-        payload: request
-    }
+  const request = axios.delete(`${ROOT_URL}/api/posts/${postId}`);
+  return {
+    type: DELETE_POST,
+    payload: request
+  };
 }
 
 export function editPost(postId, editPost) {
-    const request = axios({
-        method: 'PUT',
-        url: `${ROOT_URL}/${postId}`, 
-        data: editPost 
-    });
-    return {
-        type: EDIT_POST,
-        payload: request
-    }
+  const request = axios({
+    method: "PUT",
+    url: `${ROOT_URL}/api/posts/${postId}`,
+    data: editPost
+  });
+  return {
+    type: EDIT_POST,
+    payload: request
+  };
 }
--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,75 +1,88 @@
-import React, { Component } from 'react';
-import './index.css';
-import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
-import { Field, reduxForm } from 'redux-form';
-import Input from './Input';
-import { createPost, editPost } from '../../Actions';
-import { handleValidate as validate } from './Validate';
+import React, { Component } from "react";
+import "./index.css";
+import { connect } from "react-redux";
+import { Link, withRouter } from "react-router-dom";
+import { Field, reduxForm } from "redux-form";
+import Input from "./Input";
+import { createPost, editPost, fetchPost } from "../../Actions";
+import { handleValidate as validate } from "./Validate";
 
 class Form extends Component {
-    submit = (value) => {
-        const { id } = this.props.match.params;
-        return this.props.action === 'POST'
-        ? this.props.getPostCreate(value)
-        .then(() => {
-            this.props.history.push('/');
+  submit = value => {
+    const { id } = this.props.match.params;
+    const create_date = new Date().toLocaleString();
+    return this.props.action === "POST"
+      ? this.props.getPostCreate(value).then(() => {
+          this.props.history.push("/");
         })
-        : this.props.getPostEdit(id, {...value, create_date: new Date().toISOString()})
-        .then(() => {
-            this.props.history.push(`/posts/${id}`);
-        })
-    }
+      : this.props.getPostEdit(id, { ...value, create_date }).then(() => {
+          this.props.history.push(`/posts/${id}`);
+        });
+  };
 
-    render(){
-        const { handleSubmit } = this.props; 
-        return (
-            <div className='container'>
-                <Link to='/'>Go back to posts</Link>
-                <h4>NEW POST FORM</h4>
-                {/* pass an action creator in handleSubmit */}
-                <form onSubmit={handleSubmit(this.submit)}> 
-                    <div>
-                       <Field name='title' label='Title' component={Input} type='text'  />
-                    </div> 
-                    <div>
-                       <Field name='category' label='Category' component={Input} type='text' />
-                    </div>
-                    <div>
-                       <Field name='content' label='Write a post here' component={Input} type='textarea' />
-                    </div>
-                    <button>Save</button>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <div className="container">
+        <Link to="/">Go back to posts</Link>
+        <h4>NEW POST FORM</h4>
+        {/* pass an action creator in handleSubmit */}
+        <form onSubmit={handleSubmit(this.submit)}>
+          <div>
+            <Field name="title" label="Title" component={Input} type="text" />
+          </div>
+          <div>
+            <Field
+              name="category"
+              label="Category"
+              component={Input}
+              type="text"
+            />
+          </div>
+          <div>
+            <Field
+              name="content"
+              label="Write a post here"
+              component={Input}
+              type="textarea"
+            />
+          </div>
+          <button>Save</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    // console.log(state);
-    const { post } = state.posts;
-    return {
-        initialValues: {
-            title: post ? post.title : ' ',
-            category: post ? post.category : ' ',
-            content: post ? post.content : ' '
-        }
+  // console.log(state);
+  const { post } = state.posts;
+  return {
+    initialValues: {
+      title: post ? post.title : " ",
+      category: post ? post.category : " ",
+      content: post ? post.content : " "
     }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        getPostCreate: (newPost) => dispatch(createPost(newPost)),
-        getPostEdit: (postId, post) => dispatch(editPost(postId, post))
-    }
-}
+  return {
+    getPost: id => dispatch(fetchPost(id)),
+    getPostCreate: newPost => dispatch(createPost(newPost)),
+    getPostEdit: (postId, post) => dispatch(editPost(postId, post))
+  };
+};
 
 //connect: 1st arg is mapStateToProps, 2nd arg is mapDispatchToProps
 //reduxForm: only arg is form config
 Form = reduxForm({
-    form: 'postForm',
-    validate,
+  form: "postForm",
+  validate
 })(Form);
-Form = connect(mapStateToProps, mapDispatchToProps)(Form);
+Form = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Form);
 
 export default withRouter(Form);
